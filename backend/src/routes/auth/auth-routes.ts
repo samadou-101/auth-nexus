97a8e3314fa1a9040d2fd789429e60a25897819b
samadou <mossabm036@gmail.com>
2025-02-10 15:23:58 +0100

    Setting up ts config (ts-node, tsc)

--- backend/src/routes/auth/auth-routes.ts
+++ backend/src/routes/auth/auth-routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import sessionRouter from "./session/session-routes";
+// import sessionRouter from "./session/session-routes";
 
 const authRouter = express.Router();
+console.log("test");
 
 authRouter.get("/session", sessionRouter);
